1cff81809a986fe6fbde719b6ce4d12714ed98ab
Colin Hill <colin.james.hill@gmail.com>
2015-06-12 15:54:32 -0700

    ...

--- Engine/Source/Hect/Logic/Systems/PhysicsSystem.cpp
+++ Engine/Source/Hect/Logic/Systems/PhysicsSystem.cpp
@@ -34,6 +34,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wunused-variable"
 #endif
 
 #define BT_USE_DOUBLE_PRECISION
