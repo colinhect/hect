aeb25461db37d3a3f855c829522822db335e7ce1
Colin Hill <colin.james.hill@gmail.com>
2016-01-23 10:58:36 -0800

    Add performance tests with Celero dependency

---
+++ Engine/Tests/PerformanceTests/Source/Main.cpp
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+// This source file is part of Hect.
+//
+// Copyright (c) 2016 Colin Hill
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+#include <Hect.h>
+#include <celero/Celero.h>
+
+#include "Context.h"
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        // Use hardcoded command-line arguments for the engine to use
+        char settingsPath[] = "HectPerformanceTests.settings";
+        char* const engineArgv[] =
+        {
+            argv[0],
+            settingsPath
+        };
+
+        hect::Engine engine(2, engineArgv);
+        Context context;
+
+        celero::Run(argc, argv);
+        return 0;
+    }
+    catch (hect::Exception& exception)
+    {
+        HECT_ERROR(exception.what());
+    }
+
+    return EXIT_SUCCESS;
+}
