fd8e4cb4deb5cd4d91b416a7d247380d5a5b9a96
Colin Hill <colin.james.hill@gmail.com>
2016-08-13 11:20:44 -0700

    Remove AStyle dependency and format.py script

--- Engine/Tools/format.py
+++
@@ -1,5 +0,0 @@
-from subprocess import call
-
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\Source\\*.cpp ..\\Source\\*.h ..\\Source\\*.inl", shell=True)
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\Tests\\*.cpp ..\\Tests\\*.h ..\\Tests\\*.inl", shell=True)
-call("..\\Dependencies\\AStyle\\AStyle.exe --style=allman --recursive -n ..\\..\\Projects\\*.cpp ..\\..\\Projects\\*.h ..\\..\\Projects\\*.inl", shell=True)
